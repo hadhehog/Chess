93b6295188e1a6940bb2ffe6d88fc297d74abab6
hadhehog <52933770+hadhehog@users.noreply.github.com>
2020-07-23 17:05:35 +0200

    Pawns
    - public Pawn creates an array with X and Spaces

--- ChessCe/Pawn.cs
+++ ChessCe/Pawn.cs
@@ -6,5 +6,18 @@ using System.Threading.Tasks;
 
 namespace ChessCe
 {
- 
+    public class Pawn
+    {
+     #region WhatIsPawn
+        public const char PAWN_SYMBOL = 'X';
+        public const char SPACE = ' ';
+        public static char[,] pawns;
+     #endregion
+      
+        public Pawn()
+        {
+            pawns = new char[ChessBoard.DIMENSION, ChessBoard.DIMENSION];
+            initPawn();           //populate pawn array with X and Spaces
+        }
+    }
 }
